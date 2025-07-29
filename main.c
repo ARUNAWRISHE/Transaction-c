15b6303ce04de6e590df6444b1823cc68358c9d6
ARUNAW RISHE <arunawrishe@gmail.com>
2025-07-29 15:52:45 +0530

    .

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 #include "bank.h"
+
 static int cmp_acc(const void *a, const void *b) {
     int an = ((const Account*)a)->account_number; int bn = ((const Account*)b)->account_number; return (an - bn);
 }
@@ -12,10 +13,10 @@ GtkWidget *wit_entry_accnum, *wit_entry_pin, *wit_entry_amount;
 GtkWidget *chp_entry_accnum, *chp_entry_pin, *chp_new_pin;
 GtkWidget *del_entry_accnum, *del_entry_pin;
 GtkWidget *hist_entry_accnum, *hist_entry_pin;
-GtkWidget *admin_dialog = NULL, *admin_search_entry = NULL, *admin_details_box = NULL;
+GtkWidget *admin_dialog, *admin_search_entry, *admin_details_box;
 typedef struct { int accnum; GtkWidget *parent; } AdminHistContext;
-#define MSGBOX(dialog_parent, type, text) do { \
-    GtkWidget *dl = gtk_message_dialog_new(GTK_WINDOW(dialog_parent), GTK_DIALOG_DESTROY_WITH_PARENT, type, GTK_BUTTONS_OK, "%s", text); \
+#define MSGBOX(parent, type, text) do { \
+    GtkWidget *dl = gtk_message_dialog_new(GTK_WINDOW(parent), GTK_DIALOG_DESTROY_WITH_PARENT, type, GTK_BUTTONS_OK, "%s", text); \
     gtk_dialog_run(GTK_DIALOG(dl)); gtk_widget_destroy(dl); \
 } while(0)
 static void show_create_account(GtkWidget*, gpointer);
@@ -37,39 +38,61 @@ static void on_admin_search(GtkWidget*, gpointer);
 static void on_admin_history(GtkWidget*, gpointer);
 static void show_history(GtkWidget *, gpointer);
 static void on_history_submit(GtkWidget*, gpointer);
+
 int main(int argc, char *argv[]) {
     srand((unsigned) time(NULL));
-    GtkWidget *window, *grid;
-    GtkWidget *btn_create, *btn_viewbal, *btn_deposit, *btn_withdraw, *btn_changepin, *btn_delete, *btn_detials, *btn_exit, *btn_history;
+    GtkWidget *window, *main_vbox, *spacer_top, *spacer_between;
+    GtkWidget *btn_create, *grid_2x4;
+    GtkWidget *btn_viewbal, *btn_deposit, *btn_withdraw, *btn_changepin;
+    GtkWidget *btn_delete, *btn_detials, *btn_history, *btn_exit;
+
     gtk_init(&argc, &argv);
+
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Banking System");
-    gtk_window_set_default_size(GTK_WINDOW(window), 390, 600);
+    gtk_window_set_default_size(GTK_WINDOW(window), 480, 430);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    grid = gtk_grid_new();
-    gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
-    gtk_grid_set_row_spacing(GTK_GRID(grid), 9);
-    gtk_grid_set_column_spacing(GTK_GRID(grid), 28);
-    gtk_container_add(GTK_CONTAINER(window), grid);
-    btn_create    = gtk_button_new_with_label("Create Account");
+
+    main_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add(GTK_CONTAINER(window), main_vbox);
+
+    spacer_top = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_widget_set_size_request(spacer_top, -1, 40);
+    gtk_box_pack_start(GTK_BOX(main_vbox), spacer_top, FALSE, FALSE, 0);
+
+    btn_create = gtk_button_new_with_label("Create Account");
+    gtk_widget_set_size_request(btn_create, 220, 34);
+    gtk_widget_set_halign(btn_create, GTK_ALIGN_CENTER);
+    gtk_box_pack_start(GTK_BOX(main_vbox), btn_create, FALSE, FALSE, 0);
+
+    spacer_between = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_widget_set_size_request(spacer_between, -1, 16);
+    gtk_box_pack_start(GTK_BOX(main_vbox), spacer_between, FALSE, FALSE, 0);
+
+    grid_2x4 = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(grid_2x4), 13);
+    gtk_grid_set_column_spacing(GTK_GRID(grid_2x4), 44);
+    gtk_widget_set_halign(grid_2x4, GTK_ALIGN_CENTER);
+    gtk_box_pack_start(GTK_BOX(main_vbox), grid_2x4, TRUE, FALSE, 0);
+
     btn_viewbal   = gtk_button_new_with_label("View Balance");
-    btn_deposit   = gtk_button_new_with_label("Deposit");
-    btn_withdraw  = gtk_button_new_with_label("Withdraw");
-    btn_changepin = gtk_button_new_with_label("Change PIN");
     btn_delete    = gtk_button_new_with_label("Delete Account");
+    btn_deposit   = gtk_button_new_with_label("Deposit");
     btn_detials   = gtk_button_new_with_label("Detials (Admin)");
+    btn_withdraw  = gtk_button_new_with_label("Withdraw");
     btn_history   = gtk_button_new_with_label("See History");
+    btn_changepin = gtk_button_new_with_label("Change PIN");
     btn_exit      = gtk_button_new_with_label("Exit");
-    gtk_grid_attach(GTK_GRID(grid), btn_create,    0, 0, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), btn_viewbal,   0, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), btn_deposit,   0, 2, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), btn_withdraw,  0, 3, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), btn_changepin, 0, 4, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), btn_delete,    0, 5, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), btn_detials,   0, 6, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), btn_history,   0, 7, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), btn_exit,      0, 8, 1, 1);
+
+    gtk_grid_attach(GTK_GRID(grid_2x4), btn_viewbal,   0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid_2x4), btn_delete,    1, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid_2x4), btn_deposit,   0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid_2x4), btn_detials,   1, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid_2x4), btn_withdraw,  0, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid_2x4), btn_history,   1, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid_2x4), btn_changepin, 0, 3, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid_2x4), btn_exit,      1, 3, 1, 1);
+
     g_signal_connect(btn_create,    "clicked", G_CALLBACK(show_create_account), window);
     g_signal_connect(btn_viewbal,   "clicked", G_CALLBACK(show_view_balance), window);
     g_signal_connect(btn_deposit,   "clicked", G_CALLBACK(show_deposit), window);
@@ -79,54 +102,53 @@ int main(int argc, char *argv[]) {
     g_signal_connect(btn_detials,   "clicked", G_CALLBACK(show_admin_login), window);
     g_signal_connect(btn_history,   "clicked", G_CALLBACK(show_history), window);
     g_signal_connect(btn_exit,      "clicked", G_CALLBACK(gtk_main_quit), NULL);
+
     gtk_widget_show_all(window);
     gtk_main();
     return 0;
 }
+
 static void show_create_account(GtkWidget *widget, gpointer data) {
     GtkWidget *dialog = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(dialog), "Create New Account");
-    gtk_window_set_default_size(GTK_WINDOW(dialog), 300, 125);
+    gtk_window_set_default_size(GTK_WINDOW(dialog), 250, 90);
     GtkWidget *grid = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 6);
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 6);
     gtk_container_add(GTK_CONTAINER(dialog), grid);
-    GtkWidget *label = gtk_label_new("Enter your name:");
-    entry_name = gtk_entry_new();
-    gtk_grid_attach(GTK_GRID(grid), label,      0, 0, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), entry_name, 1, 0, 2, 1);
-    GtkWidget *btn_submit = gtk_button_new_with_label("Create");
-    gtk_grid_attach(GTK_GRID(grid), btn_submit,  1, 1, 1, 1);
-    g_signal_connect(btn_submit, "clicked", G_CALLBACK(on_create_account_submit), dialog);
+    GtkWidget *label = gtk_label_new("Enter your name:"); entry_name = gtk_entry_new();
+    gtk_grid_attach(GTK_GRID(grid), label, 0,0,1,1);
+    gtk_grid_attach(GTK_GRID(grid), entry_name, 1,0,2,1);
+    GtkWidget *btn = gtk_button_new_with_label("Create");
+    gtk_grid_attach(GTK_GRID(grid), btn, 1,1,1,1);
+    g_signal_connect(btn, "clicked", G_CALLBACK(on_create_account_submit), dialog);
     gtk_widget_show_all(dialog);
 }
 static void on_create_account_submit(GtkWidget *widget, gpointer data) {
     const char *uname = gtk_entry_get_text(GTK_ENTRY(entry_name));
-    if (strlen(uname) == 0) {
-        MSGBOX(data, GTK_MESSAGE_ERROR, "Please enter a name."); return;
-    }
+    if (strlen(uname) == 0) { MSGBOX(data, GTK_MESSAGE_ERROR, "Please enter a name."); return; }
     int accnum, pin;
-    if (!create_account_gui(uname, &accnum, &pin)) {
-        MSGBOX(data, GTK_MESSAGE_ERROR, "File error."); return;
-    }
-    char msg[256];
-    snprintf(msg, sizeof(msg), "Account created!\n\nAccount No: %d\nPIN: %d", accnum, pin);
-    MSGBOX(data, GTK_MESSAGE_INFO, msg);
-    gtk_widget_destroy(GTK_WIDGET(data));
+    if (!create_account_gui(uname, &accnum, &pin)) { MSGBOX(data, GTK_MESSAGE_ERROR, "File error."); return; }
+    char msg[128]; snprintf(msg, sizeof(msg), "Account created!\n\nAccount No: %d\nPIN: %d", accnum, pin);
+    MSGBOX(data, GTK_MESSAGE_INFO, msg); gtk_widget_destroy(GTK_WIDGET(data));
 }
 static void show_view_balance(GtkWidget *widget, gpointer data) {
     GtkWidget *dialog = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(dialog), "View Balance");
     gtk_window_set_default_size(GTK_WINDOW(dialog), 320, 130);
     GtkWidget *grid = gtk_grid_new(); gtk_container_add(GTK_CONTAINER(dialog), grid);
-    GtkWidget *label1 = gtk_label_new("Account number:"), *label2 = gtk_label_new("PIN:");
+    GtkWidget *label1 = gtk_label_new("Account number:");
     vb_entry_accnum = gtk_entry_new();
     gtk_entry_set_input_purpose(GTK_ENTRY(vb_entry_accnum), GTK_INPUT_PURPOSE_NUMBER);
-    vb_entry_pin = gtk_entry_new();
-    gtk_entry_set_visibility(GTK_ENTRY(vb_entry_pin), FALSE);
-    gtk_grid_attach(GTK_GRID(grid), label1, 0,0,1,1); gtk_grid_attach(GTK_GRID(grid), vb_entry_accnum, 1,0,1,1);
-    gtk_grid_attach(GTK_GRID(grid), label2, 0,1,1,1); gtk_grid_attach(GTK_GRID(grid), vb_entry_pin,    1,1,1,1);
-    GtkWidget *btn_submit = gtk_button_new_with_label("View");
-    gtk_grid_attach(GTK_GRID(grid), btn_submit,     1, 2, 1, 1);
-    g_signal_connect(btn_submit, "clicked", G_CALLBACK(on_view_balance_submit), dialog);
+    GtkWidget *label2 = gtk_label_new("PIN:");
+    vb_entry_pin = gtk_entry_new(); gtk_entry_set_visibility(GTK_ENTRY(vb_entry_pin), FALSE);
+    gtk_grid_attach(GTK_GRID(grid), label1, 0,0,1,1);
+    gtk_grid_attach(GTK_GRID(grid), vb_entry_accnum, 1,0,1,1);
+    gtk_grid_attach(GTK_GRID(grid), label2, 0,1,1,1);
+    gtk_grid_attach(GTK_GRID(grid), vb_entry_pin, 1,1,1,1);
+    GtkWidget *btn = gtk_button_new_with_label("View");
+    gtk_grid_attach(GTK_GRID(grid), btn, 1,2,1,1);
+    g_signal_connect(btn, "clicked", G_CALLBACK(on_view_balance_submit), dialog);
     gtk_widget_show_all(dialog);
 }
 static void on_view_balance_submit(GtkWidget *widget, gpointer data) {
@@ -134,23 +156,24 @@ static void on_view_balance_submit(GtkWidget *widget, gpointer data) {
     if (sscanf(gtk_entry_get_text(GTK_ENTRY(vb_entry_accnum)), "%d", &accnum) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(vb_entry_pin)), "%d", &pin) != 1) { MSGBOX(data, GTK_MESSAGE_ERROR, "Enter valid account number and PIN!"); return; }
     Account *acc = login_fast(accnum, pin);
     if (acc) {
-        char msg[256]; snprintf(msg, sizeof(msg), "Welcome, %s!\n\nAccount: %d\nBalance: Rs. %.2lf", acc->name, acc->account_number, acc->balance); MSGBOX(data, GTK_MESSAGE_INFO, msg);
-        gtk_widget_destroy(GTK_WIDGET(data));
-    } else {
-        MSGBOX(data, GTK_MESSAGE_ERROR, "Invalid credentials.");
-    }
+        char msg[128]; snprintf(msg, sizeof(msg), "Welcome, %s!\n\nAccount: %d\nBalance: Rs. %.2lf", acc->name, acc->account_number, acc->balance);
+        MSGBOX(data, GTK_MESSAGE_INFO, msg); gtk_widget_destroy(GTK_WIDGET(data));
+    } else { MSGBOX(data, GTK_MESSAGE_ERROR, "Invalid credentials."); }
 }
 static void show_deposit(GtkWidget *widget, gpointer data) {
     GtkWidget *dialog = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(dialog), "Deposit");
-    gtk_window_set_default_size(GTK_WINDOW(dialog), 320, 170);
+    gtk_window_set_default_size(GTK_WINDOW(dialog), 320, 160);
     GtkWidget *grid = gtk_grid_new(); gtk_container_add(GTK_CONTAINER(dialog), grid);
     GtkWidget *l1 = gtk_label_new("Account number:"); dep_entry_accnum = gtk_entry_new(); gtk_entry_set_input_purpose(GTK_ENTRY(dep_entry_accnum), GTK_INPUT_PURPOSE_NUMBER);
     GtkWidget *l2 = gtk_label_new("PIN:"); dep_entry_pin = gtk_entry_new(); gtk_entry_set_visibility(GTK_ENTRY(dep_entry_pin), FALSE);
     GtkWidget *l3 = gtk_label_new("Amount to deposit:"); dep_entry_amount = gtk_entry_new(); gtk_entry_set_input_purpose(GTK_ENTRY(dep_entry_amount), GTK_INPUT_PURPOSE_NUMBER);
-    gtk_grid_attach(GTK_GRID(grid), l1, 0,0,1,1); gtk_grid_attach(GTK_GRID(grid), dep_entry_accnum, 1,0,1,1);
-    gtk_grid_attach(GTK_GRID(grid), l2, 0,1,1,1); gtk_grid_attach(GTK_GRID(grid), dep_entry_pin,    1,1,1,1);
-    gtk_grid_attach(GTK_GRID(grid), l3, 0,2,1,1); gtk_grid_attach(GTK_GRID(grid), dep_entry_amount, 1,2,1,1);
+    gtk_grid_attach(GTK_GRID(grid), l1, 0,0,1,1);
+    gtk_grid_attach(GTK_GRID(grid), dep_entry_accnum, 1,0,1,1);
+    gtk_grid_attach(GTK_GRID(grid), l2, 0,1,1,1);
+    gtk_grid_attach(GTK_GRID(grid), dep_entry_pin,    1,1,1,1);
+    gtk_grid_attach(GTK_GRID(grid), l3, 0,2,1,1);
+    gtk_grid_attach(GTK_GRID(grid), dep_entry_amount, 1,2,1,1);
     GtkWidget *btn = gtk_button_new_with_label("Deposit");
     gtk_grid_attach(GTK_GRID(grid), btn, 1,3,1,1);
     g_signal_connect(btn, "clicked", G_CALLBACK(on_deposit_submit), dialog);
@@ -160,16 +183,14 @@ static void on_deposit_submit(GtkWidget *widget, gpointer data) {
     int accnum, pin; double amt, balance;
     if (sscanf(gtk_entry_get_text(GTK_ENTRY(dep_entry_accnum)), "%d", &accnum) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(dep_entry_pin)), "%d", &pin) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(dep_entry_amount)), "%lf", &amt) != 1 || amt <= 0) { MSGBOX(data, GTK_MESSAGE_ERROR, "Enter valid account, PIN, and positive amount!"); return; }
     if (deposit_fast(accnum, pin, amt, &balance)) {
-        char msg[128]; snprintf(msg, sizeof(msg), "Deposit successful.\nNew balance: Rs. %.2lf", balance); MSGBOX(data, GTK_MESSAGE_INFO, msg);
-        gtk_widget_destroy(GTK_WIDGET(data));
-    } else {
-        MSGBOX(data, GTK_MESSAGE_ERROR, "Account not found or PIN incorrect.");
-    }
+        char msg[128]; snprintf(msg, sizeof(msg), "Deposit successful.\nNew balance: Rs. %.2lf", balance);
+        MSGBOX(data, GTK_MESSAGE_INFO, msg); gtk_widget_destroy(GTK_WIDGET(data));
+    } else { MSGBOX(data, GTK_MESSAGE_ERROR, "Account not found or PIN incorrect."); }
 }
 static void show_withdraw(GtkWidget *widget, gpointer data) {
     GtkWidget *dialog = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(dialog), "Withdraw");
-    gtk_window_set_default_size(GTK_WINDOW(dialog), 320, 170);
+    gtk_window_set_default_size(GTK_WINDOW(dialog), 320, 160);
     GtkWidget *grid = gtk_grid_new(); gtk_container_add(GTK_CONTAINER(dialog), grid);
     GtkWidget *l1 = gtk_label_new("Account number:"); wit_entry_accnum = gtk_entry_new(); gtk_entry_set_input_purpose(GTK_ENTRY(wit_entry_accnum), GTK_INPUT_PURPOSE_NUMBER);
     GtkWidget *l2 = gtk_label_new("PIN:"); wit_entry_pin = gtk_entry_new(); gtk_entry_set_visibility(GTK_ENTRY(wit_entry_pin), FALSE);
@@ -186,15 +207,14 @@ static void on_withdraw_submit(GtkWidget *widget, gpointer data) {
     int accnum, pin; double amt, balance;
     if (sscanf(gtk_entry_get_text(GTK_ENTRY(wit_entry_accnum)), "%d", &accnum) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(wit_entry_pin)), "%d", &pin) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(wit_entry_amount)), "%lf", &amt) != 1 || amt <= 0) { MSGBOX(data, GTK_MESSAGE_ERROR, "Enter valid account, PIN, and positive amount!"); return; }
     if (withdraw_fast(accnum, pin, amt, &balance)) {
-        char msg[128]; snprintf(msg, sizeof(msg), "Withdraw successful.\nNew balance: Rs. %.2lf", balance); MSGBOX(data, GTK_MESSAGE_INFO, msg); gtk_widget_destroy(GTK_WIDGET(data));
-    } else {
-        MSGBOX(data, GTK_MESSAGE_ERROR, "Account not found, PIN incorrect, or insufficient funds.");
-    }
+        char msg[128]; snprintf(msg, sizeof(msg), "Withdraw successful.\nNew balance: Rs. %.2lf", balance);
+        MSGBOX(data, GTK_MESSAGE_INFO, msg); gtk_widget_destroy(GTK_WIDGET(data));
+    } else { MSGBOX(data, GTK_MESSAGE_ERROR, "Account not found, PIN incorrect, or insufficient funds."); }
 }
 static void show_change_pin(GtkWidget *widget, gpointer data) {
     GtkWidget *dialog = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(dialog), "Change PIN");
-    gtk_window_set_default_size(GTK_WINDOW(dialog), 340, 150);
+    gtk_window_set_default_size(GTK_WINDOW(dialog), 320, 150);
     GtkWidget *grid = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(dialog), grid);
     GtkWidget *l1 = gtk_label_new("Account number:"); chp_entry_accnum = gtk_entry_new(); gtk_entry_set_input_purpose(GTK_ENTRY(chp_entry_accnum), GTK_INPUT_PURPOSE_NUMBER);
@@ -210,7 +230,7 @@ static void show_change_pin(GtkWidget *widget, gpointer data) {
 }
 static void on_change_pin_submit(GtkWidget *widget, gpointer data) {
     int accnum, pin, newpin;
-    if (sscanf(gtk_entry_get_text(GTK_ENTRY(chp_entry_accnum)), "%d", &accnum) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(chp_entry_pin)), "%d", &pin) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(chp_new_pin)), "%d", &newpin) != 1 || newpin < 1000 || newpin > 9999) { MSGBOX(data, GTK_MESSAGE_ERROR, "Please enter valid details (PIN must be 4 digits)."); return; }
+    if (sscanf(gtk_entry_get_text(GTK_ENTRY(chp_entry_accnum)), "%d", &accnum) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(chp_entry_pin)), "%d", &pin) != 1 || sscanf(gtk_entry_get_text(GTK_ENTRY(chp_new_pin)), "%d", &newpin) != 1 || newpin < 1000 || newpin > 9999) { MSGBOX(data, GTK_MESSAGE_ERROR, "Please enter valid details (PIN must be 4 digits)."); return;}
     if (change_pin_fast(accnum, pin, newpin)) { MSGBOX(data, GTK_MESSAGE_INFO, "PIN changed successfully."); gtk_widget_destroy(GTK_WIDGET(data)); }
     else { MSGBOX(data, GTK_MESSAGE_ERROR, "Account not found or current PIN incorrect."); }
 }
@@ -264,7 +284,7 @@ static void on_admin_login_submit(GtkWidget *dialog, gpointer unused) {
 static void show_admin_details(GtkWidget *widget, gpointer data) {
     GtkWidget *dialog = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(dialog), "Admin Details Panel");
-    gtk_window_set_default_size(GTK_WINDOW(dialog), 650, 380);
+    gtk_window_set_default_size(GTK_WINDOW(dialog), 650, 390);
     GtkWidget *grid = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(dialog), grid);
     GtkWidget *lbl = gtk_label_new("Account Number:");
@@ -367,7 +387,8 @@ static void show_history(GtkWidget *widget, gpointer data) {
     GtkWidget *btn = gtk_button_new_with_label("Show");
     gtk_grid_attach(GTK_GRID(grid), btn, 1,2,1,1);
     g_signal_connect(btn, "clicked", G_CALLBACK(on_history_submit), dialog);
-    gtk_widget_show_all(dialog); }
+    gtk_widget_show_all(dialog);
+}
 static void on_history_submit(GtkWidget *widget, gpointer data) {
     int accnum, pin;
     char *accstr = (char*)gtk_entry_get_text(GTK_ENTRY(hist_entry_accnum));
